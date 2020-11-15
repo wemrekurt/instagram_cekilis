f5571313878a5748e184497502a9570b673a1bae
Emre KURT <emre.kurt@bil.omu.edu.tr>
2020-11-15 10:52:44 +0300

    Improve cekilis script

--- cekilis.rb
+++ cekilis.rb
@@ -1,20 +1,42 @@
 require 'json'
 
-file = File.read('./path/json_file.json')
-insta = JSON.parse(file)
-data = insta['GraphImages'][0]['comments']['data'].map{ |i| i['owner']['username'] }
-
-winners = []
-1.upto(35).each do |i|
-  winners << find_winners(data, winners)
-end
-
-puts 'Kazananlar: '
-puts winners
-
 def find_winners(data, winners)
   wnrs = rand(0..data.size - 1)
   return find_winners(data, winners) if winners.include?(data[wnrs])
 
   data[wnrs]
+end
+
+print 'Kullanıcı Adı: '
+username = gets.chomp
+puts '------------'
+print 'Post Kısa Kod: '
+shortcode = gets.chomp
+puts '------------'
+print 'Kazanacak Kişi Sayısı: '
+winner_size = gets.chomp.to_i
+puts '------------'
+print 'Yedek Talihli Sayısı: '
+reserve_winner_size = gets.chomp.to_i
+
+file = File.read("./#{username}/#{username}.json")
+insta = JSON.parse(file)
+data = insta['GraphImages'].select{ |i| i['shortcode'] == shortcode }.first
+
+if data
+  data = data['comments']['data'].map { |i| i['owner']['username'] }
+
+  winners = []
+  1.upto(winner_size + reserve_winner_size).each do |_i|
+    winners << find_winners(data, winners)
+  end
+
+  puts 'Kazananlar: '
+  puts winners.first(winner_size)
+  puts '------------'
+  puts 'Yedek Talihliler: '
+  puts winners.last(reserve_winner_size)
+
+else
+  puts 'Post kısa kodu bulunamadı'
 end
